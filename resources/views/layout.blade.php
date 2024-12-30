5ca0880123d8825aaff271940a7df88d1a2a7eea
owenbcoding <eoghankb@gmail.com>
2024-12-30 19:54:53 +0000

    created base layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pixel Positions</title>
+</head>
+<body>
+    <div>
+        <nav>
+            <div>logo</div>
+            <div>Links</div>
+            <div>post a job</div>
+        </nav>
+
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+</html>
