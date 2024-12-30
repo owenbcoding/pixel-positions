a2d258465043cdf8e6c346290552c0e671d8a788
owenbcoding <eoghankb@gmail.com>
2024-12-30 21:00:25 +0000

    updated vite in head + added links and some styling

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,19 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Pixel Positions</title>
-    @vite(['/resources/js/app.js'])
 </head>
-<body>
-    <div>
-        <nav>
+<body class="bg-black text-white">
+    <div class="px-10">
+        <nav class="flex justify-between items-center py-4">
             <div>
-                <a href="">
+                <a href="/">
                     <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
                 </a>
             </div>
-            <div>Links</div>
-            <div>post a job</div>
+            <div>
+                <a href="#">jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+            </div>
+            <div>
+                <a href="#">post a job</a>
+            </div>
         </nav>
 
         <main>
