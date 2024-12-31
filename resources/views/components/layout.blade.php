c06d83c9ec0165a8d6a850c19f4c6e830b450a11
owenbcoding <eoghankb@gmail.com>
2024-12-31 17:15:29 +0000

    added google fonts links and new meta tag

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600&display=swap" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body class="bg-black text-white">
     <div class="px-10">
         <nav class="flex justify-between items-center py-4 border-b border-white/10">
@@ -30,4 +35,5 @@
         </main>
     </div>
 </body>
+
 </html>
