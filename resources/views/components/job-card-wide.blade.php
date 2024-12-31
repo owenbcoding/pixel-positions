c689b2a9b1b3a934f714099396c8a187dcd4d5c3
owenbcoding <eoghankb@gmail.com>
2024-12-31 17:13:46 +0000

    created main job card layout

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,18 @@
+<div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
+    <div>
+        <img src="https://picsum.photos/seed/{{ rand(0, 1000)}}/100/100" alt="" class="rounded-xl">
+    </div>
+
+    <div class="flex-1">
+        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+
+        <h3>Video Producer</h3>
+        <p>Full time - From $60,000</p>
+    </div>
+
+    <div>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</div>
