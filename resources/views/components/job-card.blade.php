eef5decb2073597b7b556e18193e5437cb1dc9ca
owenbcoding <eoghankb@gmail.com>
2024-12-31 05:04:21 +0000

    created component for job cards

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,20 @@
+<div>
+    <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+        <div class="self-start text-sm">Laracasts</div>
+
+        <div class="py-8 font-bold">
+            <h3>Video producer</h3>
+            <p>Full time - From $60,000</p>
+        </div>
+
+        <div class="flex justify-between items-center mt-auto">
+            <div>
+                <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+                <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+                <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+            </div>
+
+            <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
+        </div>
+    </div>
+</div>
