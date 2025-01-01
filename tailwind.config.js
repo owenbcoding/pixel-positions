973ce838bc4e863efc93a16565e21c9ff0001c97
owenbcoding <eoghankb@gmail.com>
2025-01-01 18:01:46 +0000

    Add custom font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ export default {
             fontFamily: {
                 "hanken-grotesk": ["Hanken Grotesk", "sans-serif"],
             },
+            fontSize: {
+                "2xs": ".625rem", //10px
+            },
         },
     },
     plugins: [],
