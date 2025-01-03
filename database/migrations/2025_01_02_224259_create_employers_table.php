1bdf04097a06ee22fb6da823c0bab18352838cce
owenbcoding <eoghankb@gmail.com>
2025-01-03 18:50:06 +0000

    Create employers table with foreign key referencing users

--- database/migrations/2025_01_02_224259_create_employers_table.php
+++ database/migrations/2025_01_02_224259_create_employers_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -13,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('employers', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(\App\Models\User::class);
+            $table->foreignIdFor(User::class);
             $table->string('name');
             $table->timestamps();
         });
