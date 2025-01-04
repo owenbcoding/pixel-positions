117d9dcc50353c098870a72a141e0eab2d99caa0
owenbcoding <eoghankb@gmail.com>
2025-01-04 08:52:59 +0000

    created job unit test-

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -3,13 +3,16 @@
 use App\Models\Employer;
 use App\Models\Job;
 
-it('it belongs to an employer', function () {
-    // AAA
+it('belongs to an employer', function () {
     $employer = Employer::factory()->create();
     $job = Job::factory()->create([
         'employer_id' => $employer->id,
     ]);
+    expect($job->employer->is($employer))->toBeTrue();
+});
 
-    // Act and assert
-    expect ($job->employer->is($employer))->toBeTrue();
+it('can have tags', function () {
+    $job = Job::factory()->create();
+    $job->tag('Frontend');
+    expect($job->tags)->toHaveCount(1);
 });
