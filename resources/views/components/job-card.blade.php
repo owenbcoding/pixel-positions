7ab5aef49cd43b73b4e2e6d299a9d1404c7804a7
owenbcoding <eoghankb@gmail.com>
2025-01-05 17:09:28 +0000

    Refactor job card to dynamically display job and employer data

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,17 +1,17 @@
 @props(['job'])
 
 <x-panel class="flex flex-col text-center">
-    <div class="self-start text-sm">Laracasts</div>
+    <div class="self-start text-sm">{{ $job->employer->name }}</div>
 
     <div class="py-8">
-        <h3 class="group-hover:text-blue-800 text-xl font-bold">Video producer</h3>
-        <p class="text-sm mt-4">Full time - From $60,000</p>
+        <h3 class="group-hover:text-blue-800 text-xl font-bold">{{ $job->title }}</h3>
+        <p class="text-sm mt-4">{{ $job->salary }}</p>
     </div>
 
     <div class="flex justify-between items-center mt-auto">
         <div>
-            @foreach ($job->tags as $tags)
-                <x-tag :$tag size="small">Backend</x-tag>
+            @foreach ($job->tags as $tag)
+                <x-tag :$tag size="small" />
             @endforeach
         </div>
 
