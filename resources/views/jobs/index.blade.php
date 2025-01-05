d0b4d721aff8f49b5eb64c457c28679aa55f61a9
owenbcoding <eoghankb@gmail.com>
2025-01-05 01:03:21 +0000

    Refactor homepage layout to dynamically render jobs and tags using loops

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -4,44 +4,42 @@
             <h1 class="fond-bold text-4xl">Lets Find Your Next Job</h1>
 
             <form action="">
-                <input type="text" id="search" placeholder="Search for job" class="w-full mt-6 px-5 py-4 border bg-white/5 border-white/10 rounded-xl max-w-xl">
+                <input type="text" id="search" placeholder="Search for job"
+                    class="w-full mt-6 px-5 py-4 border bg-white/5 border-white/10 rounded-xl max-w-xl">
             </form>
         </section>
 
         <x-section-heading>Featured Jobs</x-section-heading>
-    
+
         <section>
             <div class="grid lg:grid-cols-3 gap-8 mt-6">
-                <x-job-card />
-                <x-job-card />
-                <x-job-card />
+                @foreach ($jobs as $job)
+                    <x-job-card :$job />
+                @endforeach
             </div>
         </section>
-    
+
         <section>
             <x-section-heading>Tags</x-section-heading>
-            
+
             <div class="mt-6 space-x-1">
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
+                @foreach ($tags as $tag)
+                    <x-tag :$tag />
+                @endforeach
+                {{-- <x-tag>Tag</x-tag> --}}
             </div>
 
         </section>
         <section>
             <x-section-heading>Recent Jobs</x-section-heading>
-            
+
             <div class="mt-6 space-y-6">
+                @foreach ($jobs as $job)
+                    <x-job-card-wide :$job />
+                @endforeach
+                {{-- <x-job-card-wide />
                 <x-job-card-wide />
-                <x-job-card-wide />
-                <x-job-card-wide />
+                <x-job-card-wide /> --}}
             </div>
         </section>
     </div>
