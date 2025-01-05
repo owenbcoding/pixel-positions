5cb9b0a0a58cb2fd622c078255acd4673a152195
owenbcoding <eoghankb@gmail.com>
2025-01-05 18:22:32 +0000

    Refactor job card wide to dynamically display job and employer data

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -6,14 +6,14 @@
     </div>
 
     <div class="flex-1 flex flex-col">
-        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+        <a href="#" class="self-start text-sm text-gray-400">{{ $job->employer->name }}</a>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800">Video Producer</h3>
-        <p class="text-sm text-gray-400 mt-auto">Full time - From $60,000</p>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800">{{ $job->title }}</h3>
+        <p class="text-sm text-gray-400 mt-auto">{{ $job->salary }}</p>
     </div>
 
     <div>
-        @foreach ($job->tags as $tags)
+        @foreach ($job->tags as $tag)
             <x-tag :$tag>Backend</x-tag>
         @endforeach
     </div>
