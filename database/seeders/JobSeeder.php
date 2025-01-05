13208817f7a1e9e197569576c5b69850d38173c7
owenbcoding <eoghankb@gmail.com>
2025-01-05 17:13:26 +0000

    added logic for job seeder

--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Job;
+use App\Models\Tag;
 
 class JobSeeder extends Seeder
 {
@@ -12,6 +14,10 @@ class JobSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $tags = Tag::factory(3)->create();
+
+        Job::factory(20)->hasAttached($tags)->create();
+    
     }
+
 }
