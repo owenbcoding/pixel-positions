cc89e7646791b6bd1f8a558b58ac77c10036a534
owenbcoding <eoghankb@gmail.com>
2025-01-07 11:27:07 +0000

    refactored register page

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -1,10 +1,14 @@
 <?php
 namespace App\Http\Controllers;
+
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rules\File;
 use Illuminate\Validation\Rules\Password;
+
 class RegisteredUserController extends Controller
 {
     /**
@@ -14,6 +18,7 @@ class RegisteredUserController extends Controller
     {
         return view('auth.register');
     }
+
     /**
      * Store a newly created resource in storage.
      */
@@ -24,17 +29,25 @@ class RegisteredUserController extends Controller
             'email' => ['required', 'email', 'unique:users,email'],
             'password' => ['required', 'confirmed', Password::min(6)],
         ]);
+
         $employerAttributes = $request->validate([
-            'employer' => ['required'],
+            'employer_name' => ['required'],
             'logo' => ['required', File::types(['png', 'jpg', 'webp'])],
         ]);
+
         $user = User::create($userAttributes);
-        $logoPath = $request->logo->store('logos');
-        $user->employer()->create([
-            'name' => $employerAttributes['employer'],
-            'logo' => $logoPath,
-        ]);
-        Auth::login($user);
-        return redirect('/');
+
+        if ($user) {
+            $logoPath = $request->file('logo')->store('logos');
+            $user->employer()->create([
+                'name' => $employerAttributes['employer_name'],
+                'logo' => $logoPath,
+            ]);
+
+            Auth::login($user);
+            return redirect('/');
+        } else {
+            return back()->withErrors(['msg' => 'User creation failed']);
+        }
     }
 }
