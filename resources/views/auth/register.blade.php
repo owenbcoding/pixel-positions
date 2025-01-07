7e9fd3b8788e935d2e617cc8fc0342d17ce216f0
owenbcoding <eoghankb@gmail.com>
2025-01-07 09:13:47 +0000

    added enctype and fixed some typos

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,16 +1,16 @@
 <x-layout>
     <x-page-heading class="text-3xl font-bold mb-6">Register</x-page-heading>
 
-    <x-forms.form mthod="POST" action="/register">
+    <x-forms.form method="POST" action="/register" enctype="multipart/form-data">
         <x-forms.input name="name" type="text" label="Name" required autofocus />
         <x-forms.input name="email" type="email" label="Email" required />
         <x-forms.input name="password" type="password" label="Password" required />
-        <x-forms.input name="password confirmation" type="password" label="password_confirmation" required />
+        <x-forms.input name="password_confirmation" type="password" label="Password Confirmation" required />
         
         <x-forms.divider />
 
-        <x-forms.input label="Employer Name" name="name" />
-        <x-forms.input label="Employer logo" name="logo" type="file"/>
+        <x-forms.input label="Employer Name" name="employer_name" />
+        <x-forms.input label="Employer Logo" name="logo" type="file"/>
 
         <x-forms.button>Create Account</x-forms.button>
     </x-forms.form>
