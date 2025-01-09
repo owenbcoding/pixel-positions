0a55778e76963ab08b4bbf26cc0c462ef01b3c80
owenbcoding <eoghankb@gmail.com>
2025-01-09 18:43:30 +0000

    Add TagController with __invoke method to display jobs by tag

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Job;
+use App\Models\Tag;
+
+class TagController extends Controller
+{
+    public function __invoke(Tag $tag)
+    {
+        return view('results', ['jobs' => $tag->jobs]);
+    }
+}
