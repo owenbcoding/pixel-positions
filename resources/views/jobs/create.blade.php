1ffb8e31a252f2c0613d31c6b75dc70934110a10
owenbcoding <eoghankb@gmail.com>
2025-01-11 08:21:36 +0000

    refactored and modified create post form page

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,20 +1,17 @@
 <x-layout>
-    <x-page-heading class="text-3xl font-bold mb-6">Post a Job</x-page-heading>
-
+    <x-page-heading>New Job</x-page-heading>
     <x-forms.form method="POST" action="/jobs">
-        @csrf
-        <x-forms.input name="title" label="Job Title" placeholder="CEO"/>
-        <x-forms.input name="salary" label="Salary" placeholder="100,000"/>
-        <x-forms.input name="location" label="Job Location"  placeholder="San Francisco, CA"/>
-    
-        <x-forms.input name="url" label="url" placeholder="https://example.com"/>
-        <x-forms.checkbox label="Feature ( Costs Extra )" name="featured" />
-
+        <x-forms.input label="Title" name="title" placeholder="CEO" />
+        <x-forms.input label="Salary" name="salary" placeholder="$90,000 USD" />
+        <x-forms.input label="Location" name="location" placeholder="Winter Park, Florida" />
+        <x-forms.select label="Schedule" name="schedule">
+            <option>Part Time</option>
+            <option>Full Time</option>
+        </x-forms.select>
+        <x-forms.input label="URL" name="url" placeholder="https://laracasts.com/work" />
+        <x-forms.checkbox label="Feature (Costs Extra)" name="featured" />
         <x-forms.divider />
-
-        <x-forms.input label="Tags (comma separated)" name="tags" placeholder="laracasts, video, education"/>
-
+        <x-forms.input label="Tags (comma separated)" name="tags" placeholder="laracasts, video, education" />
         <x-forms.button>Publish</x-forms.button>
     </x-forms.form>
-
 </x-layout>
